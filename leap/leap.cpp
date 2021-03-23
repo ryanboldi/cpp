38656886bb79e6f2de72b504b5511dea0772a7fa
Ryan Boldi <ryan.boldi123@gmail.com>
2021-03-23 14:42:51 +0400

    fixed up leap

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -2,23 +2,11 @@
 #include <iostream>
 
 namespace leap {
-    bool is_leap_year(int year) {
-        if (divisible_by(year, 4)){
-            if (divisible_by(year, 100) == false){
-                return true;
-            }
-            else if (divisible_by(year, 400)){
-                return true;
-            }
-            else {return false;}
-        }
-        else {return false;}
+    bool divisible_by(int num, int divisor) {
+        return (num % divisor == 0);
     }
 
-    bool divisible_by(int num, int divisor) {
-        if (num % divisor == 0){
-            return true;
-        }
-        return false;
+    bool is_leap_year(int year) {
+        return ((divisible_by(year, 4)) && (!divisible_by(year, 100))) || (divisible_by(year, 400));
     }
 }
