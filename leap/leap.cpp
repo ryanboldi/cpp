a207c3912cbf9103dd4da3951391b16e768e4da5
Ryan Boldi <ryan.boldi123@gmail.com>
2021-03-23 10:16:33 +0400

    finished and pass all tests

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -3,7 +3,22 @@
 
 namespace leap {
     bool is_leap_year(int year) {
-        std::cout << year;
+        if (divisible_by(year, 4)){
+            if (divisible_by(year, 100) == false){
+                return true;
+            }
+            else if (divisible_by(year, 400)){
+                return true;
+            }
+            else {return false;}
+        }
+        else {return false;}
+    }
+
+    bool divisible_by(int num, int divisor) {
+        if (num % divisor == 0){
+            return true;
+        }
         return false;
     }
 }
--- leap/leap.h
+++ leap/leap.h
@@ -3,6 +3,7 @@
 
 namespace leap {
     bool is_leap_year(int year);
+    bool divisible_by(int num, int divisor);
 }  // namespace leap
 
 #endif // LEAP_H
