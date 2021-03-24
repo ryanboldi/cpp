b3263ee4fed2e736ab06ace9bbb0a2514184b3b2
Ryan Boldi <ryan.boldi123@gmail.com>
2021-03-24 09:20:38 +0400

    passes all tests

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 namespace reverse_string {
     string reverse_string(string input){
-        return input;
+        //string is an array of chars
+
+        //construct a copy of input
+        string reversed = string(input);
+        int len = input.length();
+
+        for (int i = 0; i < len; i++){
+            reversed[i] = input[len - 1 - i];
+        }
+        return reversed;
     }
 } 
