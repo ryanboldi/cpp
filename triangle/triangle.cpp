f9ead699e26adc61e0caaff1505aa87e52205a11
Ryan Boldi <ryan.boldi123@gmail.com>
2021-04-02 21:18:20 +0400

    refactored triangle

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,15 +1,19 @@
 #include "triangle.h"
-#include <stdexcept>
 
 namespace triangle
 {
     flavor kind(double a, double b, double c)
     {
+        //create array to store the triangle sides
+        std::array<double, 3> sides = {a, b, c};
+
+        //sort the array
+        std::sort(std::begin(sides), std::end(sides));
+
         //check for illegal triangle:
-        if (a <= 0 || b <= 0 || c <= 0 || (a + b) < c || (b + c) < a || (c + a) < b)
+        if (isIllegal(sides))
         {
             throw std::domain_error("triangle invalid");
-            return flavor::illegal;
         }
         else if (a == b && b == c)
         {
@@ -23,6 +27,12 @@ namespace triangle
         {
             return flavor::scalene;
         }
+    }
+
+    bool isIllegal(std::array<double, 3> sides)
+    {
+        //check if the smallest side is shorter than 0, or that the largest side is bigger than the smaller two.
+        return (sides[0] <= 0 || sides[0] + sides[1] < sides[2]);
     }
 
 } // namespace triangle
--- triangle/triangle.h
+++ triangle/triangle.h
@@ -1,17 +1,21 @@
 #if !defined(TRIANGLE_H)
 #define TRIANGLE_H
 
+#include <stdexcept>
+#include <array>
+#include <algorithm>
+
 namespace triangle
 {
-    enum flavor
+    enum class flavor
     {
         equilateral,
         isosceles,
-        scalene,
-        illegal
+        scalene
     };
 
     flavor kind(double a, double b, double c);
+    bool isIllegal(std::array<double, 3> sides);
 } // namespace triangle
 
 #endif // TRIANGLE_H
