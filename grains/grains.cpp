cb467b0afd82883fa798d00105e769765a002af3
Ryan Boldi <ryan.boldi123@gmail.com>
2021-04-11 09:42:44 +0400

    much more efficient

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -11,11 +11,6 @@ namespace grains
 
     unsigned long long total()
     {
-        unsigned long long totalSoFar = 0;
-        for (int i = 1; i < 65; i++)
-        {
-            totalSoFar += square(i);
-        }
-        return totalSoFar;
+        return ((unsigned long long)1 << 63ULL) * 2 - 1;
     }
 } // namespace grains
