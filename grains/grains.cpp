a9b74dc299c73ac41f01d4b1e04bc052187186e4
Ryan Boldi <ryan.boldi123@gmail.com>
2021-04-12 21:00:13 +0400

    Update grains.cpp

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -6,11 +6,11 @@ namespace grains
     unsigned long long square(unsigned long long n)
     {
         //bitwise left shift AKA power of two
-        return (unsigned long long)1 << (n - 1ULL);
+        return 1ULL << (n - 1ULL);
     }
 
     unsigned long long total()
     {
-        return ((unsigned long long)1 << 63ULL) * 2 - 1;
+        return (1ULL << 63ULL) * 2 - 1;
     }
 } // namespace grains
