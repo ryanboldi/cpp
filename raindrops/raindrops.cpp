0e4318a81720a40b7540ff04f43b7f9df7d06b2b
Ryan Boldi <ryan.boldi123@gmail.com>
2021-04-12 21:10:03 +0400

    changed from if statement to a more data driven approach

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -4,18 +4,23 @@ namespace raindrops
 {
     std::string convert(int number)
     {
+
+        std::array<std::pair<int, std::string>, 3> sounds;
+        sounds[0] = std::pair<int, std::string>(3, "Pling");
+        sounds[1] = std::pair<int, std::string>(5, "Plang");
+        sounds[2] = std::pair<int, std::string>(7, "Plong");
+
         std::string toReturn = "";
 
-        if (number % 3 == 0)
-            toReturn.append("Pling");
+        for (unsigned int i = 0; i < sounds.size(); i++)
+        {
+            if (number % sounds[i].first == 0)
+            {
+                toReturn.append(sounds[i].second);
+            }
+        }
 
-        if (number % 5 == 0)
-            toReturn.append("Plang");
-
-        if (number % 7 == 0)
-            toReturn.append("Plong");
-
-        if (toReturn == "")
+        if (toReturn.empty())
         {
             return std::to_string(number);
         }
